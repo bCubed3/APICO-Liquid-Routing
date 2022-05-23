4acfc2b1052f80e5377a1cd9b955447ee64b77df
bCubed3 <sf.bcubed@gmail.com>
2022-05-22 20:34:25 -0700

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -13,14 +13,3 @@ function init()
   api_create_log("init", "pipes mod loaded !")
   return util_define_all()
 end
-
-function clock()
-  new_list = {}
-  for i=1,#PIPES do
-    if (api_inst_exists(PIPES[i])) then
-      pipes_process(PIPES[i])
-      table.insert(new_list, PIPES[i])
-    end
-  end
-  PIPES = new_list
-end
